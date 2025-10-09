1d07f2d9e640be724c4c40f71c31a1b94ff6629e
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-10-09 19:55:21 +0500

    task3 done

--- src/Task3.java
+++ src/Task3.java
@@ -22,7 +22,7 @@ public class Task3 {
     public static void main(String[] args) {
         int a=7;
         int b=5;
-        int min =a>b?a:b;
-        System.out.println(min);
+//        int min =a>b?a:b;
+//        System.out.println(min);
     }
 }
