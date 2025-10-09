cc1aa102b7d982f509dd2cb7eed076f179da65b1
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-10-09 20:01:29 +0500

    task3 done

--- src/Task3.java
+++ src/Task3.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Task3 {
     //Task3.1
 //    public static void main(String[] args) {
@@ -18,11 +20,28 @@ public class Task3 {
 //        double result =Math.pow(a,b);
 //        System.out.printf("Natija:%.2f",result);
 //    }
-    //Task3.3
+//    //Task3.3
+//    public static void main(String[] args) {
+//        int a=7;
+//        int b=5;
+
+    /// /        int min =a>b?a:b;
+    /// /        System.out.println(min);
+//    }
+    //Task3.9
     public static void main(String[] args) {
-        int a=7;
-        int b=5;
-//        int min =a>b?a:b;
-//        System.out.println(min);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("D:");
+        int d = scanner.nextInt();
+        System.out.print("V1:");
+        int v1 = scanner.nextInt();
+        System.out.print("V2:");
+        int v2 = scanner.nextInt();
+        int t =d/(v1+v2);
+        int s1 =v1*t;
+        int s2 =v2*t;
+        System.out.println("Uchratish vaqti:"+t);
+        System.out.println("Mashina1 bosib o`tgan masofa:"+s1);
+        System.out.println("Mashina2 bosib o`tgan masofa:"+s2);
     }
 }
