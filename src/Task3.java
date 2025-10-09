4b10bb1f906340a55d61d40ce9ead2d7bb64facc
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-10-09 19:54:55 +0500

    task3 done

---
+++ src/Task3.java
@@ -0,0 +1,28 @@
+public class Task3 {
+    //Task3.1
+//    public static void main(String[] args) {
+//        int a=23;
+//        int b=45;
+//        boolean res =a==b;
+//        System.out.println(res);
+//    }
+
+    //Task3.2
+    /*
+     * 7 ni 5chi darajasini ekran chaqizuvchi dasturini yozing.
+     * */
+
+    //    public static void main(String[] args) {
+//        int a=7;
+//        int b=5;
+//        double result =Math.pow(a,b);
+//        System.out.printf("Natija:%.2f",result);
+//    }
+    //Task3.3
+    public static void main(String[] args) {
+        int a=7;
+        int b=5;
+        int min =a>b?a:b;
+        System.out.println(min);
+    }
+}
