3b387e435af730ed31f8aded310295e40d3c7f4e
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-10-09 22:04:09 +0500

    task4.2 done

--- src/Task4.java
+++ src/Task4.java
@@ -3,11 +3,25 @@ import java.util.Scanner;
 public class Task4 {
 
     //Task4.1
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("n:");
+//        int n = scanner.nextInt();
+//        if (n>0){
+//            n=n+1;
+//        }
+//        System.out.println(n);
+//    }
+    //Task4.2
+    // Butun son berilgan, Agar berilgan butun son musbat bo'lsa, 1ga oshirilsin, aks 2 ga kamaytiring.
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("n:");
         int n = scanner.nextInt();
         if (n>0){
             n=n+1;
+        }else{
+            n=n-2;
         }
         System.out.println(n);
     }
