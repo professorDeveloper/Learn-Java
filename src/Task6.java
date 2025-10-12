3e23491ef25e9d19f86a60d130bb00648ed19dd9
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-10-12 16:59:46 +0500

    task6/1  done

--- src/Task6.java
+++ src/Task6.java
@@ -1,13 +1,17 @@
 public class Task6 {
+//    public static void main(String[] args) {
+//        String a = "hel";
+//        String b = "lo";
+//        String result =concatRev(a,b);
+//        System.out.println(result);
+//
+//    }
+//
+//    static String concatRev(String a, String b) {
+//        return a+b+b+a;
+//    }
+
     public static void main(String[] args) {
-        String a = "hel";
-        String b = "lo";
-        String result =concatRev(a,b);
-        System.out.println(result);
 
-    }
-
-    static String concatRev(String a, String b) {
-        return a+b+b+a;
     }
 }
