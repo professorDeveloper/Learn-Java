5fc3146c969d20b54fb281053360297015e7f5ee
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-10-13 09:22:25 +0500

    task7.6 done

--- src/Task7.java
+++ src/Task7.java
@@ -88,18 +88,33 @@ public class Task7 {
 //        return count;
 //    }
     // Task7.5
+//    public static void main(String[] args) {
+//        int n = 5;
+//        int[] arr = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            arr[i] = i + 1;
+//        }
+//        int max = arr[0];
+//        for (int i = 1; i < n; i++) {
+//            if (arr[i] > max) {
+//                max = arr[i];
+//            }
+//        }
+//        System.out.println(max);
+//    }
+    //Task7.6
     public static void main(String[] args) {
         int n = 5;
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = i + 1;
         }
-        int max = arr[0];
+        int min = arr[0];
         for (int i = 1; i < n; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
+            if (arr[i] < min) {
+                min = arr[i];
             }
         }
-        System.out.println(max);
+        System.out.println(min);
     }
 }
