a0c95c397cf93b3951bf6c9f766830b0864da178
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-10-14 20:15:45 +0500

    .task9 all done

--- src/Format.java
+++ src/Format.java
@@ -1,2 +1,8 @@
 public class Format {
+    public static void main(String[] args) {
+        System.out.println(String.format("%s %s", "Hello", "World"));
+        String name ="Hack";
+        int age = 20;
+        System.out.println(String.format("%s is %d years old", name, age));
+    }
 }
