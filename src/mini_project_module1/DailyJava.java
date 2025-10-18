25184e66c0a1056c11449c80c6a954b1b004247a
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-10-18 09:52:13 +0500

    i forgot yesterday commit why its really sad

--- src/mini_project_module1/DailyJava.java
+++ src/mini_project_module1/DailyJava.java
@@ -3,23 +3,12 @@ package mini_project_module1;
 public class DailyJava {
     public static void main(String[] args) {
         int[] arr = {5, 2, 8, 3, 1, 6, 4};
-        bubbleSort(arr);
         for (int i : arr) {
             System.out.print(i + " ");
         }
         System.out.println();
-    }
-    ///
+//    code removed
 
-    public static void bubbleSort(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
     }
+
 }
