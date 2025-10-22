f676627a3f5fc79691e7070b61b6b58ed7b18748
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-10-22 10:35:11 +0500

    module2 task1.2 task1.1 done

---
+++ src/oop_learn/Constructors.java
@@ -0,0 +1,18 @@
+package oop_learn;
+
+public class Constructors {
+    public static void main(String[] args) {
+//        var people = new People();
+
+    }
+}
+
+
+class People {
+    public String name;
+    public int age;
+
+    public People() {
+        System.out.print("ePop");
+    }
+}
