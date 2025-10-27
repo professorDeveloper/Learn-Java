a9f9aa2d03a0d1bbe9154f41963322dfcbf8a2af
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-10-27 14:25:35 +0500

    module2 task1.2 task1.1 done

---
+++ src/inheritance/Iphone13.java
@@ -0,0 +1,41 @@
+package inheritance;
+
+public class Iphone13 extends Iphone {
+    private int ram;
+    private int storage;
+
+    public Iphone13(String name, String model, String color, int chargingTime, String version, int ram, int storage) {
+        super(name, model, color, chargingTime, version);
+        this.ram = ram;
+        this.storage = storage;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public int getStorage() {
+        return storage;
+    }
+
+    public void setStorage(int storage) {
+        this.storage = storage;
+    }
+
+    @Override
+    public String toString() {
+        return "Iphone13{" +
+                "ram=" + ram +
+                ", storage=" + storage +
+                ", version='" + super.getVersion() + '\'' +
+                ", name='" + super.getName() + '\'' +
+                ", model='" + super.getModel() + '\'' +
+                ", color='" + super.getColor() + '\'' +
+                ", chargingTime=" + super.getChargingTime() +
+                '}';
+    }
+}
---
+++ src/inheritance_3/Phone.java
@@ -0,0 +1,48 @@
+package inheritance_3;
+
+import java.util.Objects;
+
+public class Phone {
+    private String name;
+    private String color;
+    private String storage;
+
+    public Phone(String name, String color, String storage) {
+        this.name = name;
+        this.color = color;
+        this.storage = storage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getStorage() {
+        return storage;
+    }
+
+    public void setStorage(String storage) {
+        this.storage = storage;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Phone phone = (Phone) o;
+        return Objects.equals(name, phone.name) && Objects.equals(color, phone.color) && Objects.equals(storage, phone.storage);
+    }
+
+
+}
