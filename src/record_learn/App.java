f2725a2899945fee448e3dab709e2db7b5ca4d7a
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-02 22:30:02 +0500

    learning record class

--- src/record_learn/App.java
+++ src/record_learn/App.java
@@ -1,4 +1,12 @@
 package record_learn;
 
+import java.time.LocalDate;
+
 public class App {
+    public static void main(String[] args) {
+        EmployeeRecord employeeRecord = new EmployeeRecord(1, "mohamed", LocalDate.of(2021, 1, 1));
+        EmployeeDTO employeeDTO = new EmployeeDTO(1, "mohamed", LocalDate.of(2021, 1, 1));
+
+        System.out.println(employeeRecord.json());
+    }
 }
--- src/record_learn/EmployeeRecord.java
+++ src/record_learn/EmployeeRecord.java
@@ -2,4 +2,9 @@ package record_learn;
 
 import java.time.LocalDate;
 
-public record  EmployeeRecord(Integer id , String fullName, LocalDate hiredDate) {};
+public record  EmployeeRecord(Integer id , String fullName, LocalDate hiredDate) {
+
+    public String json(){
+        return "{ id:%d, fullName:%s, hiredDate:%s } ".formatted(id, fullName, hiredDate);
+    }
+};
