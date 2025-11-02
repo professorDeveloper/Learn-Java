eb08e97ce338f79a2c6f43d9f84f18de8044c785
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-02 22:33:24 +0500

    learning record class

--- src/record_learn/App.java
+++ src/record_learn/App.java
@@ -5,7 +5,8 @@ import java.time.LocalDate;
 public class App {
     public static void main(String[] args) {
         EmployeeRecord employeeRecord = new EmployeeRecord(1, "mohamed", LocalDate.of(2021, 1, 1));
-        EmployeeDTO employeeDTO = new EmployeeDTO(1, "mohamed", LocalDate.of(2021, 1, 1));
+        EmployeeRecord employeeRecordThrow = new EmployeeRecord(-1, "Azamov", LocalDate.of(2023, 1, 1));
+//        EmployeeDTO employeeDTO = new EmployeeDTO(1, "mohamed", LocalDate.of(2021, 1, 1));
 
         System.out.println(employeeRecord.json());
     }
--- src/record_learn/EmployeeRecord.java
+++ src/record_learn/EmployeeRecord.java
@@ -4,6 +4,13 @@ import java.time.LocalDate;
 
 public record  EmployeeRecord(Integer id , String fullName, LocalDate hiredDate) {
 
+    public EmployeeRecord(Integer id, String fullName, LocalDate hiredDate) {
+        if (id<0) throw new IllegalArgumentException("id must be positive number");
+        this.id = id;
+        this.fullName = fullName;
+        this.hiredDate = hiredDate;
+    }
+
     public String json(){
         return "{ 'id':'%d', 'fullName':'%s', 'hiredDate':'%s' } ".formatted(id, fullName, hiredDate);
     }
