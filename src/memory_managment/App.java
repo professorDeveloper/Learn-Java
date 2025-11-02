2eccf54326d61c1f04a4e19820cfd1202985ca22
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-02 18:19:00 +0500

    lesson 2.7 done

--- src/memory_managment/App.java
+++ src/memory_managment/App.java
@@ -4,9 +4,14 @@ public class App {
     public static void main(String[] args) {
         Integer i = 10;
         App app = new App();
+        // with integer
         app.m1(i);
         System.out.println(i);
         Counter ct = new Counter();
+        // Counter data  first changed
+        ct.counter = 252;
+        System.out.println(ct.counter);
+        //Counter data  changed with Passing value
         app.m2(ct);
         System.out.println(ct.counter);
     }
---
+++ src/memory_managment/garbage_collection/App.java
@@ -0,0 +1,7 @@
+package memory_managment.garbage_collection;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
