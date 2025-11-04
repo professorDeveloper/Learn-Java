ec427c713aaea557a66dd76cbf8976943f73cd67
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-04 10:11:11 +0500

    fm design patterns

---
+++ src/design_patterns/Employee.java
@@ -0,0 +1,14 @@
+package design_patterns;
+
+public class Employee {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String  fatherName;
+    private Integer age;
+    private String email;
+    private String phoneNumber;
+    private String position;
+
+}
