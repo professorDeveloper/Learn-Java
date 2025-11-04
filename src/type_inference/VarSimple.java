1728b67d6037c7173b3e0758d15d246b451b8ef2
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-04 08:58:39 +0500

    Type inference

---
+++ src/type_inference/VarSimple.java
@@ -0,0 +1,13 @@
+package type_inference;
+
+public class VarSimple {
+    public static void main(String[] args) {
+        var name = "John";
+        var age = 20;
+        var isMarried = false;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(isMarried);
+//        var
+    }
+}
