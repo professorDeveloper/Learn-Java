73b7c23bba3bd7b71a2a4414e26b6bfe702774af
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-04 10:19:44 +0500

    fm design patterns

--- src/design_patterns/Main.java
+++ src/design_patterns/Main.java
@@ -1,8 +1,14 @@
 package design_patterns;
 
+import design_patterns.builder.Employee;
+import design_patterns.factory_method.FactoryMethod;
+
 public class Main {
     public static void main(String[] args) {
         String str = String.valueOf(2255);
         FactoryMethod fm = FactoryMethod.getInstance();
+        Employee emp = new Employee.EmployeeBuilder().id("1").firstName("John").lastName("Doe").build();
+        System.out.println(emp);
+//        emp
     }
 }
--- src/design_patterns/factory_method/FactoryMethod.java
+++ src/design_patterns/factory_method/FactoryMethod.java
@@ -1,4 +1,4 @@
-package design_patterns;
+package design_patterns.factory_method;
 
 public class FactoryMethod {
     private FactoryMethod() {
