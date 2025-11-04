dfc3231659880ece626daab9041bd6fbd5e8760d
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-04 10:32:53 +0500

    singleton design patterns

--- src/design_patterns/singleton/Singleton.java
+++ src/design_patterns/singleton/Singleton.java
@@ -1,10 +1,16 @@
 package design_patterns.singleton;
 
 public class Singleton {
+    //    private static Singleton instance = new Singleton(); eager initialization
+    private static Singleton instance = null; // lazy initialization
+
     private Singleton() {
     }
 
     public static Singleton getInstance() {
-        return new Singleton();
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance; //lazy initialization
     }
 }
