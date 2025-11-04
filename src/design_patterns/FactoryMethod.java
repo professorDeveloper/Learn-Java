0fbf3e8d0e6827c72e1f336a58fc9296cd84ad60
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-04 09:22:26 +0500

    fm design patterns

--- src/design_patterns/FactoryMethod.java
+++ src/design_patterns/FactoryMethod.java
@@ -1,4 +1,12 @@
 package design_patterns;
 
 public class FactoryMethod {
+    private FactoryMethod() {
+        System.out.println("Calling private constructor");
+    }
+
+    public static FactoryMethod getInstance() {
+        return new FactoryMethod();
+    }
+
 }
---
+++ src/design_patterns/Main.java
@@ -0,0 +1,8 @@
+package design_patterns;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = String.valueOf(2255);
+        FactoryMethod fm = FactoryMethod.getInstance();
+    }
+}
