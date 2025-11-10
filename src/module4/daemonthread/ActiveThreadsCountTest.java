cfc9a71dbec572cecbf254d864dae1ac3a4b3a7b
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-10 12:18:39 +0500

    ThreadInterruption

---
+++ src/module4/daemonthread/ActiveThreadsCountTest.java
@@ -0,0 +1,13 @@
+package module4.daemonthread;
+
+public class ActiveThreadsCountTest {
+    public static void main(String[] args) {
+        int x = Thread.activeCount();
+        Thread[] threads = new Thread[x];
+        Thread.enumerate(threads);
+        System.out.println(x);
+        for (Thread thread : threads) {
+            System.out.println(thread);
+        }
+    }
+}
