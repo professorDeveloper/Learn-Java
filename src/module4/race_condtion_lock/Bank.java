8391d2df94a52585c8c1f265ab8eec874ff27470
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-10 20:21:28 +0500

    Race Condition problem fixed

---
+++ src/module4/race_condtion_lock/Bank.java
@@ -0,0 +1,37 @@
+package module4.race_condtion_lock;
+
+import java.util.Arrays;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Bank {
+    private final double[] accounts;
+    private final Lock lock = new ReentrantLock();
+
+    public Bank(int n, double initialBalance) {
+        this.accounts = new double[n];
+        Arrays.fill(accounts, initialBalance);
+    }
+
+    public void transfer(int from, int to, double amount) {
+        if (accounts[from] < amount) return;
+        lock.lock();
+        System.out.print(Thread.currentThread() + ": ");
+        accounts[from] -= amount;
+        System.out.printf("%10.2f from %d to %d ", amount, from, to);
+        accounts[to] += amount;
+        System.out.printf("Total balance: %10.2f%n", getTotalBalance());
+        lock.unlock();
+    }
+
+    public double getTotalBalance() {
+        double sum = 0;
+        for (double a : accounts) sum += a;
+        return sum;
+    }
+
+    public int size() {
+        return accounts.length;
+    }
+
+}
--- src/module4/race_condtion_lock/RaceConditionProblemSolved.java
+++ src/module4/race_condtion_lock/RaceConditionProblemSolved.java
@@ -1,8 +1,6 @@
-package module4.race_condition;
+package module4.race_condtion_lock;
 
-import java.util.Arrays;
-
-public class RaceConditionProblem {
+public class RaceConditionProblemSolved {
     public static final int N_ACCOUNTS = 100;
     public static final double INITIAL_BALANCE = 1000;
     public static final double AMOUNT = 100;
@@ -30,31 +28,3 @@ public class RaceConditionProblem {
     }
 }
 
-class Bank {
-    private final double[] accounts;
-
-    public Bank(int n, double initialBalance) {
-        this.accounts = new double[n];
-        Arrays.fill(accounts, initialBalance);
-    }
-
-    public void transfer(int from, int to, double amount) {
-        if (accounts[from] < amount) return;
-        System.out.print(Thread.currentThread() + ": ");
-        accounts[from] -= amount;
-        System.out.printf("%10.2f from %d to %d", amount, from, to);
-        accounts[to] += amount;
-        System.out.printf("Total balance: %10.2f%n", getTotalBalance());
-    }
-
-    public double getTotalBalance() {
-        double sum = 0;
-        for (double a : accounts) sum += a;
-        return sum;
-    }
-
-    public int size() {
-        return accounts.length;
-    }
-
-}
