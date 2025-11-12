59ecc07ccf28c1bd2c012604c487dc7d87cbef38
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-12 18:02:24 +0500

    imutable learning

---
+++ src/module4/imutable/EmployeeTest.java
@@ -0,0 +1,10 @@
+package module4.imutable;
+
+public class EmployeeTest {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Address address = new Address("Tashkent", "Chilonzor");
+        Employee employee = new Employee("John Doe", 30, address);
+      /*  address.setCity("Bishkek");
+        System.out.println("Employee: " + employee);*/
+    }
+}
