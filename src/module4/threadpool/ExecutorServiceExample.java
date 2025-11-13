92f37081ffda9089a7c4dc507beca634e1a48878
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-13 10:53:43 +0500

    thread pools

--- src/module4/threadpool/ExecutorServiceExample.java
+++ src/module4/threadpool/ExecutorServiceExample.java
@@ -7,9 +7,15 @@ public class ExecutorServiceExample {
     public static void main(String[] args) throws InterruptedException {
         ExecutorService service = Executors.newFixedThreadPool(10);
         for (int i = 0; i < 200; i++) {
-            Runnable r = () -> System.out.println("Thread " + Thread.currentThread().getName() + " is running");
+            final int i1 = i;
+            Runnable r = () -> System.out.println("Thread " + Thread.currentThread().getName() + " is running"+ "taskID: " + i1);
             Thread.sleep(5);
             service.execute(r);
         }
+        service.execute(()->{
+            System.out.println(
+                    "Fuck all"
+            );
+        });
     }
 }
