0d75212670dbd7d9a350000e3b076b3611afa207
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-13 10:49:52 +0500

    immutable learning

--- src/module4/threadpool/ExecutorServiceExample.java
+++ src/module4/threadpool/ExecutorServiceExample.java
@@ -1,4 +1,15 @@
 package module4.threadpool;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ExecutorServiceExample {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 200; i++) {
+            Runnable r = () -> System.out.println("Thread " + Thread.currentThread().getName() + " is running");
+            Thread.sleep(5);
+            service.execute(r);
+        }
+    }
 }
