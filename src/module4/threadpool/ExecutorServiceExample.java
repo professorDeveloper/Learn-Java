70b8535e5eadf1678ce8520d4e5943a1b76dcee9
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-14 09:12:31 +0500

    thread pools

--- src/module4/threadpool/ExecutorServiceExample.java
+++ src/module4/threadpool/ExecutorServiceExample.java
@@ -1,21 +1,31 @@
 package module4.threadpool;
 
+import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ExecutorServiceExample {
     public static void main(String[] args) throws InterruptedException {
+//        ExecutorService service = Executors.newFixedThreadPool(10); // Fixed orqali ochilgan thread pool   appda exception bo`lmasa u o`lmaydi.
+//        for (int i = 0; i < 200; i++) {
+//            final int i1 = i;
+//            Runnable r = () -> System.out.println("Thread " + Thread.currentThread().getName() + " is running"+ "taskID: " + i1);
+//            Thread.sleep(5);
+//            service.execute(r);
+//        }
+////        service.execute(()->{
+////            System.out.println(
+////                    "Fuck all"
+////            );
+////        });
         ExecutorService service = Executors.newFixedThreadPool(10);
         for (int i = 0; i < 200; i++) {
             final int i1 = i;
-            Runnable r = () -> System.out.println("Thread " + Thread.currentThread().getName() + " is running"+ "taskID: " + i1);
             Thread.sleep(5);
-            service.execute(r);
+            Runnable r = () -> System.out.println("Thread " + Thread.currentThread().getName() + " is running" + "taskID: " + i1 + " Date: " + new Date().toString());
+            service.submit(r);
         }
-        service.execute(()->{
-            System.out.println(
-                    "Fuck all"
-            );
-        });
+
+        service.shutdown();
     }
 }
