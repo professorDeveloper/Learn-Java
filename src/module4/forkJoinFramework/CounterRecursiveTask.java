c73b369302b19ccb597183b91921518e3b7b3e92
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-16 17:38:03 +0500

    forkJoinExample

---
+++ src/module4/forkJoinFramework/CounterRecursiveTask.java
@@ -0,0 +1,36 @@
+package module4.forkJoinFramework;
+
+import java.util.concurrent.RecursiveTask;
+
+public class CounterRecursiveTask extends RecursiveTask<Integer> {
+    double[] numbers;
+    private int from;
+    private int to;
+
+    private int limit = 1000;
+
+    public CounterRecursiveTask(double[] numbers, int from, int to) {
+        this.numbers = numbers;
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    protected Integer compute() {
+        if (this.to - this.from < limit) {
+            int count = 0;
+            for (int i = this.from; i < this.to; i++) {
+                if (numbers[i] > 0.3) {
+                    count++;
+                }
+            }
+            return count;
+        } else {
+            int middle = this.from + (this.to - this.from) / 2;
+            CounterRecursiveTask first = new CounterRecursiveTask(numbers, this.from, middle);
+            CounterRecursiveTask second = new CounterRecursiveTask(numbers, middle, this.to);
+            invokeAll(first, second);
+            return first.join() + second.join();
+        }
+    } // Compute methodi task qachon bo`linishi kerak va qahcon bo`linmasligi kerak degan algorithm yozish kk
+}
