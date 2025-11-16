84a184979ee154dc9dd5be42f3c4439df16757b2
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-16 22:37:11 +0500

    async completable future example

--- src/module4/completable_future/CompletableFutureExample.java
+++ src/module4/completable_future/CompletableFutureExample.java
@@ -7,7 +7,7 @@ import java.util.concurrent.Executors;
 
 public class CompletableFutureExample {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        // one way for creating completable future
+      /*  // one way for creating completable future
         CompletableFuture<String> completableFuture = new CompletableFuture<>();
         completableFuture.complete("Hello");
         System.out.println(completableFuture.get());
@@ -22,6 +22,43 @@ public class CompletableFutureExample {
                 throw new RuntimeException(e);
             }
             System.out.println("Hello FromRunAsync");
-        }, executorService);
+        }, executorService);*/
+        register();
+        System.out.println(returnSuccessMessage());
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Message " + i);
+            Thread.sleep(20);
+        }
+    }
+
+    private static String returnSuccessMessage() {
+        try {
+            Thread.sleep(500);
+            return "Sucessfully Registered Check Your Email";
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void register() {
+        ExecutorService service = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        CompletableFuture.runAsync(() -> {
+            System.out.println("User data sent to database :: Thread: " + Thread.currentThread().getName());
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("User data saved to database :: Thread: " + Thread.currentThread().getName());
+        }, service);
+        CompletableFuture.runAsync(() -> {
+            System.out.println("Email Sending :: Thread: " + Thread.currentThread().getName());
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Email Sent :: Thread: " + Thread.currentThread().getName());
+        }, service);
     }
 }
