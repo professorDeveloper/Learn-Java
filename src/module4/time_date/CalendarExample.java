1ec7b2d8a679f6c92c2f2db01a56a7862cb0ca78
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-17 20:33:28 +0500

    calendar abstraction learning

--- src/module4/time_date/CalendarExample.java
+++ src/module4/time_date/CalendarExample.java
@@ -6,5 +6,6 @@ public class CalendarExample {
     public static void main(String[] args) {
         Calendar calendar = Calendar.getInstance();
         System.out.println(calendar.getTime());
+        System.out.println(calendar.getFirstDayOfWeek());
     }
 }
