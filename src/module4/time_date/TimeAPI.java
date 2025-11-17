4b16e274c5505cc1c541bd0e5c36b17b1908377b
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-17 21:48:29 +0500

    Time API learning

--- src/module4/time_date/TimeAPI.java
+++ src/module4/time_date/TimeAPI.java
@@ -4,6 +4,7 @@ import java.time.Clock;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjusters;
 import java.util.Date;
@@ -13,6 +14,10 @@ public class TimeAPI {
 //        System.out.println(Instant.now().get(ChronoField.DAY_OF_MONTH));
         LocalDate now = LocalDate.now();
         System.out.println(now);
+        System.out.println(now.isLeapYear());
+        System.out.println(now.plusYears(3).isLeapYear());
+        System.out.println(now.plusDays(22)); // now time -> 11.17.25 -> after 22 days -> 12.09.25
+
     }
 
 }
