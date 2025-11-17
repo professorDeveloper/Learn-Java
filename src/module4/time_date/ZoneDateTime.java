2a93cfaac8ac5a2f6675bb520b3cf218b6424e4d
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-17 21:54:51 +0500

    Time API learning

---
+++ src/module4/time_date/ZoneDateTime.java
@@ -0,0 +1,14 @@
+package module4.time_date;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class ZoneDateTime {
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        ZoneId.getAvailableZoneIds().forEach(System.out::println);
+        System.out.println(now.getZone().getDisplayName(TextStyle.FULL, Locale.CANADA));
+    }
+}
