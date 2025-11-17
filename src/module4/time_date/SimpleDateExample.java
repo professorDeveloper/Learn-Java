74120ee517cbd547ae4ef0b65517ad7d39fd3fe1
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-17 21:19:02 +0500

    calendar simpleDate example

--- src/module4/time_date/SimpleDateExample.java
+++ src/module4/time_date/SimpleDateExample.java
@@ -1,10 +1,19 @@
 package module4.time_date;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class SimpleDateExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        format();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String formatedDate="23/03/2020";
+       Date date =simpleDateFormat.parse(formatedDate);
+        System.out.println(date);
+    }
+
+    private static void format() {
         Date date =new Date();
         System.out.println(date);
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd 'helloo' HH:mm:ss");
