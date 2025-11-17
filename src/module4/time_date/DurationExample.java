c1e53da979fe94c740e670c287a442b50676a9fd
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-17 22:02:29 +0500

    Time API learning

---
+++ src/module4/time_date/DurationExample.java
@@ -0,0 +1,14 @@
+package module4.time_date;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class DurationExample {
+    public static void main(String[] args) {
+        LocalDateTime dateTime =LocalDateTime.of(1995,1,25,6,30,10);
+
+        LocalDateTime now = LocalDateTime.now();
+        Duration duration = Duration.between(dateTime,now);
+        System.out.println(duration.toDays());
+    }
+}
