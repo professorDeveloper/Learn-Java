df96b9113cc60410680accbcf4b966393b5de73d
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-17 20:06:56 +0500

    date time java

--- src/module4/time_date/Main.java
+++ src/module4/time_date/Main.java
@@ -1,4 +1,11 @@
 package module4.time_date;
 
+import java.util.Date;
+
 public class Main {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+    }
 }
+
