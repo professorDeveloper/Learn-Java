dfcd90ec7b6d9d090401566962150a6545db9479
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-17 09:46:59 +0500

    async learn with calculator

--- src/module4/completable_future/CompletableFutureExample.java
+++ src/module4/completable_future/CompletableFutureExample.java
@@ -7,22 +7,6 @@ import java.util.concurrent.Executors;
 
 public class CompletableFutureExample {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-      /*  // one way for creating completable future
-        CompletableFuture<String> completableFuture = new CompletableFuture<>();
-        completableFuture.complete("Hello");
-        System.out.println(completableFuture.get());
-
-        // second way for using completable future;
-        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-        CompletableFuture.runAsync(() -> {
-            System.out.println("Running Thread: " + Thread.currentThread().getName());
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("Hello FromRunAsync");
-        }, executorService);*/
         register();
         System.out.println(returnSuccessMessage());
         for (int i = 0; i < 10; i++) {
---
+++ src/module4/completable_future/ThenCombineAnyOf.java
@@ -0,0 +1,5 @@
+package module4.completable_future;
+
+public class ThenCombineAnyOf {
+
+}
