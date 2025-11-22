a24d8e6ccbb495ef9c3546182d96465a26870369
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-22 19:28:25 +0500

    Loggin learning

---
+++ src/module4/lessons/logging/SimpleLoggingExample.java
@@ -0,0 +1,21 @@
+package module4.lessons.logging;
+
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class SimpleLoggingExample {
+    private static final Logger logger = Logger.getLogger("MyLogger");
+
+    public static void main(String[] args) {
+        ConsoleHandler handler = new ConsoleHandler();
+        SimpleFormatter simpleFormatter = new SimpleFormatter();
+        logger.setUseParentHandlers(false);
+        logger.addHandler(handler);
+        handler.setLevel(Level.INFO);
+        
+        handler.setFormatter(simpleFormatter);
+        logger.log(Level.SEVERE, "This is an info message");
+    }
+}
