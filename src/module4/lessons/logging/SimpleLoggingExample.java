f6315b8dc8d8b0a9783f5b0f56a0e971bf114987
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-22 22:35:09 +0500

    logging properties added

--- src/module4/lessons/logging/SimpleLoggingExample.java
+++ src/module4/lessons/logging/SimpleLoggingExample.java
@@ -3,17 +3,20 @@ package module4.lessons.logging;
 import java.util.logging.*;
 
 public class SimpleLoggingExample {
+    static  {
+        String file = SimpleLoggingExample.class.getClassLoader().getResource("logging.properties").getFile();
+        System.setProperty("java.util.logging.config.file", file);
+    }
     private static final Logger logger = Logger.getLogger("MyLogger");
 
     public static void main(String[] args) {
+        System.out.println(System.getProperty("java.util.logging.config.file"));
         ConsoleHandler handler = new ConsoleHandler();
         SimpleFormatter simpleFormatter = new SimpleFormatter();
         logger.setUseParentHandlers(false);
         logger.addHandler(handler);
-        handler.setLevel(Level.INFO);
-
         handler.setFormatter(simpleFormatter);
-        LogRecord logRecord = new LogRecord(Level.INFO, "Hello world");
+        LogRecord logRecord = new LogRecord(Level.FINE, "Hello world");
         logger.log(logRecord);
 
     }
