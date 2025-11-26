bb7920a5c3fd9059710ef72e6003455d04289aa0
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-26 15:19:42 +0500

    custom error formatter removed

--- src/module4/projects/ucell/dto/UserRegisterDTO.java
+++ src/module4/projects/ucell/dto/UserRegisterDTO.java
@@ -16,9 +16,6 @@ public class UserRegisterDTO {
         return fullName;
     }
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
 
     public String getEmail() {
         return email;
