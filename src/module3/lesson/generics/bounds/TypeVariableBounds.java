25a57a692802889bceeb1df13339c1e928344608
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-28 11:28:55 +0500

    Type bounds example

--- src/module3/lesson/generics/bounds/TypeVariableBounds.java
+++ src/module3/lesson/generics/bounds/TypeVariableBounds.java
@@ -8,10 +8,25 @@ public class TypeVariableBounds<T extends Number> {
     }
 }
 
+interface CanConnectToDatabase {
+}
+
+class Repository implements CanConnectToDatabase {
+}
+
+class Service<R extends CanConnectToDatabase> {
+    private final R repository;
+
+    Service(R repository) {
+        this.repository = repository;
+    }
+}
+
 class TypeVariableBoundsTest {
     public static void main(String[] args) {
         var t = new TypeVariableBounds<Integer>(12);
         var t2 = new TypeVariableBounds<>(12L);
         var t3 = new TypeVariableBounds<>(12.0);
+        var service = new Service<>(new Repository());
     }
 }
