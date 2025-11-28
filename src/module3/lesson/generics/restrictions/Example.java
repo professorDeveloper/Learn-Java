1c36321d043700323db2ae5b2abe43e806b982c3
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-28 12:14:53 +0500

    Task3 done

---
+++ src/module3/lesson/generics/restrictions/Example.java
@@ -0,0 +1,8 @@
+package module3.lesson.generics.restrictions;
+
+public class Example<E> {
+    public static void main(String[] args) {
+        var r = new Example<Integer>();
+    }
+
+}
---
+++ src/module3/lesson/generics/task2/Horse.java
@@ -0,0 +1,9 @@
+package module3.lesson.generics.task2;
+
+public class Horse extends Animal {
+
+    Horse(String name, String color) {
+        super(color, name);
+
+    }
+}
