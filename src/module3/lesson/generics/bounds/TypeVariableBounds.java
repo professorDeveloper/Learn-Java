5a9e7168140e8e03afbd1819c233f4aff2c21c88
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-11-28 11:26:12 +0500

    Type bounds example

--- src/module3/lesson/generics/bounds/TypeVariableBounds.java
+++ src/module3/lesson/generics/bounds/TypeVariableBounds.java
@@ -1,8 +1,17 @@
 package module3.lesson.generics.bounds;
 
-public class TypeVariableBounds {
+public class TypeVariableBounds<T extends Number> {
+    private final T field;
+
+    public TypeVariableBounds(T field) {
+        this.field = field;
+    }
 }
 
 class TypeVariableBoundsTest {
-
+    public static void main(String[] args) {
+        var t = new TypeVariableBounds<Integer>(12);
+        var t2 = new TypeVariableBounds<>(12L);
+        var t3 = new TypeVariableBounds<>(12.0);
+    }
 }
