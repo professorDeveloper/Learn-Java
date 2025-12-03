8def14c1406720c944e2957afc505f3dbc8d7ee8
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-12-03 14:31:16 +0500

    set learning...

--- src/module3/lesson/set/ObjectCreating.java
+++ src/module3/lesson/set/ObjectCreating.java
@@ -6,5 +6,6 @@ import java.util.Set;
 public class ObjectCreating {
     public static void main(String[] args) {
         Set<String> languages =new HashSet<>();
+        languages.add("a");
     }
 }
