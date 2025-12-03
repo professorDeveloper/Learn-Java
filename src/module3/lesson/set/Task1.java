a9c69f866270bf501b17519f29b904089bf9d676
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-12-03 15:05:15 +0500

    set learning...

--- src/module3/lesson/set/Task1.java
+++ src/module3/lesson/set/Task1.java
@@ -3,11 +3,8 @@ package module3.lesson.set;
 import java.util.HashSet;
 import java.util.Set;
 
-public class ObjectCreating {
+public class Task1 {
     public static void main(String[] args) {
-/*
-        a dan z gacha bo'lgan harflarni saqlovchi set yarating forEach orqali ekranga chop eting
-*/
         Set<String> set = new HashSet<>();
         for (char c = 'a'; c <= 'z'; c++) {
             set.add(String.valueOf(c));
