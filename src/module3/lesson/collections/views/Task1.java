2395a230e93df52001725ad6dfb693874c6bd62f
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-12-06 16:47:09 +0500

    lesson7 lesson8 done

--- src/module3/lesson/collections/views/Task1.java
+++ src/module3/lesson/collections/views/Task1.java
@@ -2,12 +2,10 @@ package module3.lesson.collections.views;
 
 import java.util.ArrayList;
 import java.util.List;
-
 class Car {
     String name;
     String color;
     String model;
-
     public Car(String name, String color, String model) {
         this.name = name;
         this.color = color;
