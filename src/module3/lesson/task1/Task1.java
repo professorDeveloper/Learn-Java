67fa4811ecc40e6dd8fad97f41d7a3f90ecbfc30
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-12-10 09:30:20 +0500

    java learning..

---
+++ src/module3/lesson/task1/Task1.java
@@ -0,0 +1,13 @@
+package module3.lesson.task1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        /*Ismlardan iborat uzunligi yigirma bo'lgan massiv yarating
+        Uni Collection listiga o'giring va shuffle qiling
+        listni soritng qilib optionalga orab bervoring
+        optional bo'sh bo'lmasa optionaldan listni olib list elementlarini chop eting
+*/
+
+
+    }
+}
